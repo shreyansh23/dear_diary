d00e0c3639190bf9ad3976505ac7ca2e9059f987
Shre23 <jain.shreyansh0@gmail.com>
2018-06-23 15:22:51 +0530

    app completed

--- entries/views.py
+++ entries/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Entry
+from .forms import EntryForm
 
 def index(request):
     template = 'entries/index.html'
-    return render(request,template)
+    entry_object_list = Entry.objects.order_by('-date_posted')
+    context = {'entry_object_list':entry_object_list}
+    return render(request,template,context)
 
 def add(request):
-    template = 'entries/add.html'
-    return render(request,template)
+
+    if request.method == 'POST':
+        print(0)
+        form = EntryForm(request.POST)
+        print (1)
+
+        if form.is_valid():
+            print(2)
+            form.save()
+            print(3)
+            return redirect('home')
+
+
+    else:
+        form = EntryForm()
+        context = {'form':form}
+        template = 'entries/add.html'
+        return render(request,template,context)
